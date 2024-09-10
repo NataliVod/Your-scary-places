742c6de7e3bd774478003148fda914ac83f7631b
NataliVod <n3824@yandex.ru>
2024-09-11 00:43:18 +0300

    add resources

--- app/src/main/java/com/bloodmoon/yourscaryplaces/ui/theme/Color.kt
+++ app/src/main/java/com/bloodmoon/yourscaryplaces/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryColor = Color(0xFF1C1F1E)        // #1C1F1E
+val OnPrimaryColor = Color(0xFFC8392E)       // #C8392E
+val SecondaryColor = Color(0xFF804E4E)       // #804E4E
+val OnSecondaryColor = Color(0xFFFFFFFF)     // #FFFFFF
+val SurfaceColor = Color(0xFF804E4E)         // #804E4E
+val OnSurfaceColor = Color(0xFFE1E1E1)       // #E1E1E1
---
+++ app/src/main/java/com/bloodmoon/yourscaryplaces/ui/theme/Shape.kt
@@ -0,0 +1,9 @@
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+object JetYourscaryplacesShape {
+    val small = RoundedCornerShape(8.dp)
+    val medium = RoundedCornerShape(16.dp)
+    val large = RoundedCornerShape(32.dp)
+    val dialog = RoundedCornerShape(8.dp)
+}
